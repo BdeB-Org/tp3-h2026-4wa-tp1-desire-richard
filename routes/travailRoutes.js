6abad73f1f344eec7d267321fb56434d37b54b64
Charlottericc <richardcharliericc@gmail.com>
2026-05-04 09:37:02 -0400

    Index travail changer

--- routes/travailRoutes.js
+++ routes/travailRoutes.js
@@ -5,19 +5,19 @@ const travailController = require("../controllers/travailController");
 const authMiddleware = require("../middleware/authMiddleware");
 
 // route GET
-router.get("/travail/", authMiddleware, travailController.getTravail);
+router.get("/", authMiddleware, travailController.getTravail);
 
 // route GET ID
-router.get("/travail/:id", authMiddleware, travailController.getTravailByID);
+router.get("/:id", authMiddleware, travailController.getTravailByID);
 
 // route POST
-router.post("/travail/", authMiddleware, travailController.addTravail);
+router.post("/", authMiddleware, travailController.addTravail);
 
 // route UPDATE
-router.put("/travail/:id", authMiddleware, travailController.updateTravail);
+router.put("/:id", authMiddleware, travailController.updateTravail);
 
 // route DELETE
-router.delete("/travail/:id", authMiddleware, travailController.deleteTravail);
+router.delete("/:id", authMiddleware, travailController.deleteTravail);
 
 // VAR : Auteure = Charlotte Richard
 module.exports = router;
